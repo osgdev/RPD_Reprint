83fd0c880ec7278388bef9bccaf0a8d0f69021be
Pete <swanseatechsupport@hotmail.co.uk>
2019-04-15 14:07:21 +0100

    Removed AppCreds from RestClient

--- src/main/java/gov/dvla/osg/reprint/network/RestClient.java
+++ src/main/java/gov/dvla/osg/reprint/network/RestClient.java
@@ -10,10 +10,7 @@ import org.apache.logging.log4j.Logger;
 import org.glassfish.jersey.media.multipart.MultiPart;
 import org.glassfish.jersey.media.multipart.MultiPartFeature;
 
-import gov.dvla.osg.reprint.models.AppCredentials;
-import gov.dvla.osg.reprint.models.Config;
 import gov.dvla.osg.reprint.models.Session;
-import gov.dvla.osg.reprint.utils.JsonUtils;
 
 /**
  * Utility methods to transmit messages to the RPD REST service.
@@ -61,35 +58,13 @@ public class RestClient {
 	 * @param multiPart Should contain the file(s) to transmit
 	 * @return 202 status code if file was transmitted successfully
 	 */
-	public static Response rpdSubmit(String url, MultiPart multiPart) {
-        /*********** Retrieve token for the AIW user ****************/
-        AppCredentials appCredentials = null;
-
-        try {
-            String loginUrl = Config.getProtocol() + Config.getHost() + ":" + Config.getPort() + Config.getLoginUrl();
-            
-            appCredentials = new AppCredentials();
-            
-            Response response = ClientBuilder.newClient()
-                .target(loginUrl)
-                .queryParam("name", appCredentials.getUsername())
-                .queryParam("pwd", appCredentials.getPassword())
-                .request(MediaType.APPLICATION_JSON)
-                .get();
-            
-            String data = response.readEntity(String.class);
-            appCredentials.setToken(JsonUtils.getTokenFromJson(data));
-        } catch (Exception ex) {
-            LOGGER.fatal("Unable to log Application into RPD. Check password is valid.", ex.getMessage());
-        }
-        /**********************************************************/
-	    
+	public static Response rpdSubmit(String url, MultiPart multiPart) {  
 		// Send file using the APP token
 		return ClientBuilder.newClient()
 				.register(MultiPartFeature.class)
 				.target(url)
 				.request(MediaType.APPLICATION_JSON)
-		        .header("ippdcredential", "<credential token='" + appCredentials.getToken() + "'/>")
+		        .header("ippdcredential", "<credential token='" + Session.getToken() + "'/>")
 		        .post(Entity.entity(multiPart, multiPart.getMediaType()));
 	}
 	
